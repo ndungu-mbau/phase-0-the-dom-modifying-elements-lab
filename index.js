2255acdd97ef3839ead1924ae2d40d1be95b7484
Nelson Mbau <mbau.ndungu@gmail.com>
2024-03-28 10:21:09 +0300

    feat: add code to manipulate dom

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+
+const mainElem = document.querySelector("main#main");
+document.body.removeChild(mainElem);
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = `Nelson is the champion`;
+
+document.body.appendChild(newHeader);
